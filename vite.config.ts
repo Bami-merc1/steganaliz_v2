b44fc1e4e6c90fdf0d1b873707e64aeb738572a4
Bamidele Mercy Enoch <mercoly2006@gmail.com>
2026-09-10 16:34:23 +0100

    commit . .

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'http://steganaliz-v2.onrender.com:4000',
         changeOrigin: true,
       },
     },
